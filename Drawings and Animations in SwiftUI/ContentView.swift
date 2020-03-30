dfcf6910b68edda30a2d1fdc9fd81ab9f28faff8
Roberto Liccardo <roberto_liccardo@outlook.it>
2020-03-30 17:06:40 +0200

    Initial Commit

---
+++ Drawings and Animations in SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Drawings and Animations in SwiftUI
+//
+//  Created by Roberto Liccardo on 30/03/2020.
+//  Copyright © 2020 Roberto Liccardo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
